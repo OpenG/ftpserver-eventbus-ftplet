a3b0679485649309585e4203d5c624f3eee5e0d8
Kęstutis Triponis <kestutis@openg.lt>
2015-05-12 16:29:36 +0300

    Created basic Ftplet

---
+++ src/test/java/eu/openg/ftpserver/ftplet/EventBusFtpletTest.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2015 OpenG (Atvira Karta, LLC)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.openg.ftpserver.ftplet;
+
+import org.apache.ftpserver.ftplet.Ftplet;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+import static org.junit.Assert.assertThat;
+
+public class EventBusFtpletTest {
+
+    @Test
+    public void shouldImplementFtplet() {
+        assertThat(new EventBusFtplet(), is(instanceOf(Ftplet.class)));
+    }
+}
